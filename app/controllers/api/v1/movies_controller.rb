0d0e56916f8ee383ee893b3cd9962a18b17b0624
aminmallol <aminm-34@hotmail.fr>
2022-07-28 17:40:07 +0100

    Managed to make the first api call from the react app

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::MoviesController < ActionController::API
   # ! Hash out the below if you don't want to authenticate
-  before_action :authenticate_user!, only: [ :index ]
+  # before_action :authenticate_user!, only: [ :index ]
 
   def index
-    if user_signed_in?
-      render json: Movie.all
-    else
-      render json: {}, status: 401
-    end
+    render json: Movie.all
+    # if user_signed_in?
+    # else
+    #   render json: {}, status: 401
+    # end
   end
 
 end
