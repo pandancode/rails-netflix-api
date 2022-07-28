c860504b618bd3b1713354986f424e429160c8b5
yilun chen <yilun.chen@web.de>
2022-07-28 15:29:27 +0100

    Added user_details

---
+++ test/factories/user_details.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user_detail do
+    title { "MyString" }
+    first_name { "MyString" }
+    middle_names { "MyString" }
+    last_name { "MyString" }
+    user { nil }
+  end
+end
