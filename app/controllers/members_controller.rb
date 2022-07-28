72696bc83d06512babd3a2f7f085a9ec9e87fee7
yilun chen <yilun.chen@web.de>
2022-07-28 21:26:44 +0100

    Put in all the auth token skippers to make the guide work

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,9 @@
 class MembersController < ApplicationController
+  # ! Amin, the below explicitly (and for only, ONLY) disables the CSRF token. To be discussed and
+  # ! reviewed as per second answer from the below
+  # ! https://stackoverflow.com/questions/35181340/rails-cant-verify-csrf-token-authenticity-when-making-a-post-request
+  skip_before_action :verify_authenticity_token, only: [:show]
+
   before_action :authenticate_user!
 
   def show
