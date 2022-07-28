07e911d44f1cb2c05d19fb09f1a3f092db91d6d7
yilun chen <yilun.chen@web.de>
2022-07-28 19:00:56 +0100

    Unfortunately, the Application Controller is now set in API mode - maybe it works for you fully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
-class ApplicationController < ActionController::Base
+# class ApplicationController < ActionController::Base
+#   before_action :authenticate_user!
+# end
+
+
+# ! Below code is from
+# ! https://medium.com/@mazik.wyry/rails-5-api-jwt-setup-in-minutes-using-devise-71670fd4ed03
+
+class ApplicationController < ActionController::API
   before_action :authenticate_user!
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+      errors: [
+        {
+          status: '400',
+          title: 'Bad Request',
+          detail: resource.errors,
+          code: '100'
+        }
+      ]
+    }, status: :bad_request
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   resources :movies
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
+
   root to: "pages#home"
 
   namespace :api do
