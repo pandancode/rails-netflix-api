4d43aa06fccb57c823abc60858cd848a275f3e04
yilun chen <yilun.chen@web.de>
2022-07-28 15:28:31 +0100

    Fixed reference thing

--- db/migrate/20220728142822_create_user_details.rb
+++ db/migrate/20220728142822_create_user_details.rb
@@ -3,8 +3,9 @@ class CreateUserDetails < ActiveRecord::Migration[7.0]
     create_table :user_details do |t|
       t.string :title
       t.string :first_name
-      t.string :middle_name
+      t.string :middle_names
       t.string :last_name
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- test/factories/user_details.rb
+++ test/factories/user_details.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user_detail do
     title { "MyString" }
     first_name { "MyString" }
-    middle_name { "MyString" }
+    middle_names { "MyString" }
     last_name { "MyString" }
+    user { nil }
   end
 end
