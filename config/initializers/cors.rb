18916c45f35b64598843a34f437091f2ed2e5177
aminmallol <aminm-34@hotmail.fr>
2022-08-10 11:59:52 +0100

    Favourite movie list is working perfectly (GET / POST/ DELETE). Soon going to start working with watchlists.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins "localhost:3006"
 
     resource '*',
              headers: :any,
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :movies
-      resources :favorites, except: [:index ]
+      resources :favorites
     end
   end
 
