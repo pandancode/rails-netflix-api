9d4aac7a52b0080a3fd62c732f1fe7a53ec119cc
aminmallol <aminm-34@hotmail.fr>
2022-08-10 17:03:05 +0100

    Everything works perfectly. Wrote notes about how to read json / send data to client as json.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -14,17 +14,17 @@ class Api::V1::FavoritesController < ActionController::API
 
     if new_favorite.save!
       favorite_list = current_user.movies
-      render json: { message: "Added the movie with id #{res_body["movie_id"]} as favorite for user #{current_user.email}", favorite_movies: favorite_list }, status: :ok
+      render json: { message: "Added the movie with id #{res_body["movie_id"]} as favorite for user #{current_user.email}", favorite_movies: favorite_list.to_json }, status: :ok
     else
-      render json: { message: "An error occured while adding movie #{res_body["movie_id"]} for user  #{current_user.email}", favorite_movies: favorite_list }, status: :unprocessable_entity
+      render json: { message: "An error occured while adding movie #{res_body["movie_id"]} for user  #{current_user.email}", favorite_movies: favorite_list.to_json }, status: :unprocessable_entity
     end
   end
 
 
   def destroy
-    # res_body = JSON.parse(request.raw_post)
-    p params["id"]
     movie_to_delete = Favorite.where(user_id: current_user.id).where(movie_id: params["id"])[0]
     movie_to_delete.delete
+    favorite_list = current_user.movies
+    render json: { message: "Movie with id #{params["id"]} deleted from favorite successfully for user #{current_user.email}", favorite_movies: favorite_list.to_json }, status: :ok
   end
 end
